78f48c7e699489842611b843f882e05522255ba4
helloGalina <togerasimova@gmail.com>
2025-12-09 18:52:35 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,57 @@
+import { useRef } from "react";
+
+import moon from "./assets/moon.png";
+import land from "./assets/land.png";
+import cat from "./assets/cat.gif";
+
+import { Parallax, ParallaxLayer } from "@react-spring/parallax";
+
+function App() {
+  const ref = useRef();
+
+  return (
+    <div className="app-wrapper">
+      <Parallax pages={4} ref={ref}>
+
+        <ParallaxLayer
+          offset={0}
+          speed={1}
+          factor={2}
+          className="moon-bg"
+          style={{
+            backgroundImage: `url(${moon})`,
+            backgroundSize: "cover",
+          }}
+        />
+
+        <ParallaxLayer
+          offset={2}
+          speed={1}
+          factor={4}
+          style={{
+            backgroundImage: `url(${land})`,
+            backgroundSize: "cover",
+          }}
+        />
+
+        <ParallaxLayer
+          sticky={{ start: 0.9, end: 2.5 }}
+          style={{ textAlign: "center" }}
+        >
+          <img src={cat} alt="cat" style={{ width: "300px" }} />
+        </ParallaxLayer>
+
+        <ParallaxLayer offset={0.2} speed={0.05} onClick={() => ref.current.scrollTo(3)}>
+          <h2 className="big-text">Welcome to my website</h2>
+        </ParallaxLayer>
+
+        <ParallaxLayer offset={3} speed={2} onClick={() => ref.current.scrollTo(0)}>
+          <h2 className="big-text">Hi Mom!</h2>
+        </ParallaxLayer>
+
+      </Parallax>
+    </div>
+  );
+}
+
+export default App;
